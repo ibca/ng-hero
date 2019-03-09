d5cbccaa6e52d87956f4b28ca2bb2fe3b76b03ea
Jack Yang <ibca@163.com>
2019-03-09 13:48:12 +0800

    angular ng generate

---
+++ src/app/hero-detial/hero-detial.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+
+@Component({
+  selector: 'app-hero-detial',
+  templateUrl: './hero-detial.component.html',
+  styleUrls: ['./hero-detial.component.scss']
+})
+export class HeroDetialComponent implements OnInit {
+
+  @Input() hero: Hero;
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
