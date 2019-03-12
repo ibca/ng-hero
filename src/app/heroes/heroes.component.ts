7bf7b70aae144797428dad879bfaa9b9122a2346
Jack Yang <ibca@163.com>
2019-03-12 11:13:58 +0800

    fiexed delete hero replace

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -41,7 +41,7 @@ export class HeroesComponent implements OnInit {
 
   delete(hero: Hero): void {
 
-    this.heroes.filter(h => h !== hero);
+    this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
 
